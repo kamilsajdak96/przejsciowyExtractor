b7c6d1051c49de857ace42352933608ad0feed6f
Kamil <kamilsajdak96@gmail.com>
2020-01-18 18:37:11 +0100

    refactoring controller - moving decorators structures from app.py to controller directory

--- app.py
+++ app.py
@@ -1,35 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask
 from helpers.RunScheduler import runScheduler
 from controllers.GIOSController import *
 
 app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return jsonify({"hehe": "Hello World!"})
-
-@app.route('/measuring_stations', methods=['GET'])
-def executeGetMeasuringStations():
-    return getMeasuringStations()
-
-@app.route('/measuring_positions/<stationId>', methods=['GET'])
-def executeGetMeasuringPositions(stationId):
-    return getMeasuringPositions(stationId)
-
-@app.route('/measuring_data/<sensorId>', methods=['GET'])
-def executeGetMeasuringData(sensorId):
-    return getMeasuringData(sensorId)
-
-@app.route('/air_index/<stationId>', methods=['GET'])
-def executeGetAirIndex(stationId):
-    return getAirIndex(stationId)
-
-@app.route('/predicting_results', methods=['GET'])
-def executePrediction():
-    return prediction()
+app.register_blueprint(gios_controller)
 
 runScheduler()
 
 if __name__ == '__main__':
     app.run()
-
--- controllers/GIOSController.py
+++ controllers/GIOSController.py
@@ -1,19 +1,33 @@
+from flask import Blueprint
+
 from enums.ERROR import ERROR
 from enums.URL import *
-from services.PredictingService import prediction
 from services.GIOSService import constructUrl
+from services.PredictingService import prediction
 
+gios_controller = Blueprint('gios_controller', __name__)
+
+
+@gios_controller.route('/measuring_stations', methods=['GET'])
 def getMeasuringStations():
     return constructUrl(URL.API_MEASURING_STATIONS.value)
 
+
+@gios_controller.route('/measuring_positions/<stationId>', methods=['GET'])
 def getMeasuringPositions(stationId):
     return constructUrl(URL.API_MEASURING_POSITIONS.value, ERROR.NO_SUCH_STATION_ID.value, stationId)
 
+
+@gios_controller.route('/measuring_data/<sensorId>', methods=['GET'])
 def getMeasuringData(sensorId):
     return constructUrl(URL.API_MEASURING_DATA.value, ERROR.NO_SUCH_SENSOR_ID.value, sensorId)
 
+
+@gios_controller.route('/air_index/<stationId>', methods=['GET'])
 def getAirIndex(stationId):
     return constructUrl(URL.API_AIR_INDEX.value, ERROR.NO_SUCH_STATION_ID.value, stationId)
 
+
+@gios_controller.route('/predicting_results', methods=['GET'])
 def mlResults():
     return prediction()
